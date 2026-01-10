0a31a38527eb09a6e62f7399f674de7849631a3f
seshasrinivasvemula <seshasrinivas_vemula@srmap.edu.in>
2026-01-10 13:36:00 +0530

    Add files via upload

---
+++ Stringlength.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    char str[] = "Hello";
+    char *ptr = str;
+    int length = 0;
+
+    while (*ptr != '\0') {
+        length++;
+        ptr++;
+    }
+    printf("Output: %d", length);
+ return 0;
+}
